3e6a10ba579b9a7bc102d9a3924efcb03e0ecd51
Miroslava Vybiralova <miroslava.vybiralova@gmail.com>
2024-02-26 16:03:58 +0100

    Moje reseni

---
+++ index.js
@@ -0,0 +1,22 @@
+//Novinky
+//1
+const bodyElement = document.querySelector("body")
+bodyElement.style.backgroundColor = "#e9e9e9"
+
+//2
+const newsElement = document.querySelector(".news")
+newsElement.style.backgroundColor = "white"
+newsElement.style.scrollWidth = "2rem" 
+
+//3
+const h1Element = document.querySelector("h1")
+h1Element.classList.add("news__title")
+h1Element.textContent = "Aktuální novinky"
+
+//4
+const zprava1Element = document.querySelector("#zprava1")
+zprava1Element.classList.add("post--main")
+
+//5 - nefunguje
+const zprava3Element = document.querySelector("#zprava3")
+zprava3Element.src = "images/zprava3-novy.jpg"
